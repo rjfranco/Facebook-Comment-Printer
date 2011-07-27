8f25c0a2d15b4b24b17fcf3d8cffb905cf88fec9
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-27 11:47:54 -0700

    Committing initial copy of new Facebook Comment Printer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbCommentPrinter::Application.config.secret_token = 'b2c6420c43027acf29661f44dba35732dafd148a3d4bd9ea690abd74ab8a1358eeb5b946f8b5a40922c24bc0bdf6b384865431695aa29c82e4720fe4796f4253'
