0f55ba7d3a27afd678fc445ebb1de8e0ae49df19
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-27 17:10:34 -0700

    Adding stubs for modules

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,4 +4,11 @@ module PagesHelper
     agent.user_agent_alias = 'Mac Safari'
     agent.get(thisurl).title
   end
+  
+  def get_comments_for(thisurl)
+    
+  end
+  
+  def get_json_from_facebook_for(thisurl)
+  end
 end
